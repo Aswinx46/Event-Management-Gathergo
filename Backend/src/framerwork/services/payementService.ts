3d063f62657c8d9c9f52234e290f6002501a6772
Aswinx46 <aswinx46@gmail.com>
2025-05-15 19:28:59 +0530

    ymal file updated

--- Backend/src/framerwork/services/payementService.ts
+++ Backend/src/framerwork/services/payementService.ts
@@ -5,7 +5,7 @@ import { TicketFromFrontend } from "../../domain/entities/Ticket/ticketFromFrote
 export class PaymentService implements IStripeService {
     private stripe: Stripe
     constructor() {
-        this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+        this.stripe = new Stripe(process.env.STRIPE_SK!, {
             apiVersion: "2025-03-31.basil"
         })
     }
